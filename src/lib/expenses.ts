6ec416842afd6b57ad378717daca0fa2cc07095f
stivadar <tivadar.simon01@gmail.com>
2025-01-28 15:53:31 +0100

    tsignore

--- src/lib/expenses.ts
+++ src/lib/expenses.ts
@@ -1,10 +1,12 @@
 import prisma from '@/lib/prisma'
 
 export async function getExpenses() {
+  // @ts-ignore: Prisma Client nem találja az 'expense' modellt
   return await prisma.expense.findMany()
 }
 
 export async function createExpense(data: { amount: number; title: string }) {
+  // @ts-ignore: Prisma Client nem találja az 'expense' modellt
   return await prisma.expense.create({
     data
   })
